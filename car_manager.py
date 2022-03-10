d99a731bf21770ccba59a070ba3636ab9080f417
CatBook2010 <vkhaaneev@gmail.com>
2022-03-10 12:25:15 +0100

    Create and Move cars

--- car_manager.py
+++ car_manager.py
@@ -8,13 +8,13 @@ MOVE_INCREMENT = 10
 
 class CarManager:
     def __init__(self):
-        all_cars = []
+        self.all_cars = []
     
     def create_car(self):
-        random_chance = random.randomint(1,6)
+        random_chance = random.randint(1,6)
 
         if random_chance == 1:
-            new_car = Turtle()
+            new_car = Turtle("square")
             new_car.shapesize(stretch_wid=1,stretch_len=2)
             new_car.penup()
             new_car.color(random.choice(COLORS))
--- player.py
+++ player.py
@@ -7,4 +7,11 @@ FINISH_LINE_Y = 280
 
 class Player(Turtle):
     def __init__(self):
-        su
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+    
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
